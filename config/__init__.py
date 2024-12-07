27c11bbb770e48e3d62ee9ed3f9887d54757ec96
Lev Chernikov <108144614+sodiNICH@users.noreply.github.com>
2024-12-08 01:43:48 +0300

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, unicode_literals
 
-# Это необходимо для того, чтобы Celery был загружен при старте Django
 from .celery import app as celery_app
 
 __all__ = ('celery_app',)
